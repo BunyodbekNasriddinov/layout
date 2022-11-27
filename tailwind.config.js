28b8ae75c4a76395cd1203bf5c2e488ab2c8cad6
BunyodbekNasriddinov <nasriddinoffbunyodbek@gmail.com>
2022-11-27 15:56:42 +0500

    Adwars page hero section add

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   content: ["./src/**/*.html"],
   theme: {
     extend: {
+      colors: {
+        textColor: "#424242",
+        bgBtnColor: "#D8B152"
+      },
+      fontFamily: {
+        openSans: "'Open Sans'"
+      }
     },
   },
   plugins: [],
