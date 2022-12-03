72bd355170e971950157d23e0db269b79a7c36ee
= <93827769+BunyodbekNasriddinov@users.noreply.github.com>
2022-12-03 15:43:22 +0500

    Publication page add

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,59 +2,48 @@ module.exports = {
   content: ["./src/**/*.html"],
   theme: {
     extend: {
-
-
-      
-    
       colors: {
-        'btn': '#AEA38B',
+        btn: "#AEA38B",
         textColor: "#424242",
         bgBtnColor: "#D8B152",
-        proektText: '#424242;',
-        contactBg: '#AEA38B',
-        imgBg: '#625245',
-        aboutText: '#424242',
-        'tcolor': '#424242',
-        'btncolor': '#AEA38B',
+        proektText: "#424242;",
+        contactBg: "#AEA38B",
+        imgBg: "#625245",
+        aboutText: "#424242",
+        tcolor: "#424242",
+        btncolor: "#AEA38B",
+        bordercolor: "#AEA38B"
       },
-    
 
-       
-      
       fontFamily: {
-        openSans: "'Open Sans'"
+        openSans: "'Open Sans'",
       },
       width: {
-        'imgwidth': '360px',
-        familyContent: '860px',
-        familyContentTablet: '740px',
-        familyContentMobile: '300px',
-        aboutImgWidth: '480px'
+        imgwidth: "360px",
+        familyContent: "860px",
+        familyContentTablet: "740px",
+        familyContentMobile: "300px",
+        aboutImgWidth: "480px",
       },
-  
+
       backgroundImage: {
-        'hero-pattern': "url('/1-2-3-page-image/header-bg.jpg')",
-        'uslug1': "url('/1-2-3-page-image/uslug-bg1.jpg')",
-        'uslug2': "url('/1-2-3-page-image/uslug-bg2.jpg')",
-        'uslug3': "url('/1-2-3-page-image/uslug-bg3.jpg')",
+        "hero-pattern": "url('/1-2-3-page-image/header-bg.jpg')",
+        uslug1: "url('/1-2-3-page-image/uslug-bg1.jpg')",
+        uslug2: "url('/1-2-3-page-image/uslug-bg2.jpg')",
+        uslug3: "url('/1-2-3-page-image/uslug-bg3.jpg')",
 
-        'card1': "url('/1-2-3-page-image/card1.jpg')",
-        'card2': "url('/1-2-3-page-image/card2.jpg')",
-        'card3': "url('/1-2-3-page-image/card3.jpg')",
-        'card4': "url('/1-2-3-page-image/card4.jpg')",
-        'card5': "url('/1-2-3-page-image/card5.jpg')",
-        'card6': "url('/1-2-3-page-image/card6.jpg')",
-        'card7': "url('/1-2-3-page-image/card7.jpg')",
-        'card8': "url('/1-2-3-page-image/card8.jpg')",
-        'tel': "url('/1-2-3-page-image/telefon.svg')",
-        'email': "url('/1-2-3-page-image/email.svg')",
-        
-       
-      
-
-
+        card1: "url('/1-2-3-page-image/card1.jpg')",
+        card2: "url('/1-2-3-page-image/card2.jpg')",
+        card3: "url('/1-2-3-page-image/card3.jpg')",
+        card4: "url('/1-2-3-page-image/card4.jpg')",
+        card5: "url('/1-2-3-page-image/card5.jpg')",
+        card6: "url('/1-2-3-page-image/card6.jpg')",
+        card7: "url('/1-2-3-page-image/card7.jpg')",
+        card8: "url('/1-2-3-page-image/card8.jpg')",
+        tel: "url('/1-2-3-page-image/telefon.svg')",
+        email: "url('/1-2-3-page-image/email.svg')",
+      },
     },
   },
-  },
   plugins: [],
-}
+};
